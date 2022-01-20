fed80abc404d6642f3019a84c6c88ab4344f6179
RavinduSathsara <rsathsara330@gmail.com>
2022-01-20 08:37:49 +0530

    Install MUI

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <div
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <h1>Hello</h1>
+      </div>
     </div>
   );
 };
