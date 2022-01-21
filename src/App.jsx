963b8efe3bc5e9d8a218e3d863e752d79c32918c
RavinduSathsara <rsathsara330@gmail.com>
2022-01-21 15:13:51 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,77 @@
-import React from "react";
+import { useState } from "react";
 
-const App = () => {
+function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ["/", "*", "-", "+", "."];
+
+  const updateClac = (value) => {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+
+    setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button onClick={() => updateClac(i.toString())} key={i}>
+          {i}
+        </button>
+      );
+    }
+    return digits;
+  };
+
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  };
+
+  const deletelast = () => {
+    if (calc === "") {
+      return;
+    } else {
+      const value = calc.slice(0, -1);
+      setCalc(value);
+    }
+  };
+
   return (
-    <div>
-      <div
-        style={{
-          position: "absolute",
-          left: "50%",
-          top: "50%",
-          transform: "translate(-50%, -50%)",
-        }}
-      >
-        <h1>Hello</h1>
+    <div className="App">
+      <div className="calculator">
+        <div className="display">
+          {result ? <span>({result})</span> : ""}
+          {calc || "0"}
+        </div>
+        <div className="operators">
+          <button onClick={() => updateClac("/")}>/</button>
+          <button onClick={() => updateClac("*")}>*</button>
+          <button onClick={() => updateClac("-")}>-</button>
+          <button onClick={() => updateClac("+")}>+</button>
+
+          <button onClick={deletelast}>DEL</button>
+        </div>
+
+        <div className="digits">
+          {createDigits()}
+          <button onClick={() => updateClac("0")}>0</button>
+          <button onClick={() => updateClac(".")}>.</button>
+          <button onClick={calculate}>=</button>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default App;
